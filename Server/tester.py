c61feb25f19e1e652404faa97062cc8c15a180d2
Unknown <adshayan28@gmail.com>
2017-03-21 14:00:22 +0100

    server
    funker, men ikke fornoyd

---
+++ Server/tester.py
@@ -0,0 +1,17 @@
+validRequests = ['login', 'msg', 'logout', 'names', 'help']
+
+
+# request form
+request = {
+                'request': request,
+                'content': content,
+            }
+
+
+
+
+
+
+
+
+
